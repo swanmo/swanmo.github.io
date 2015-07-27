d4593a4a9806f84dac12ce981923c55e417850dc
swanmo <swanmo@gmail.com>
2015-07-27 23:49:50 +0200

    Remove triggas från tangenttryckning

--- fabric-labs/assets/js/tools/remove-tool.js
+++ fabric-labs/assets/js/tools/remove-tool.js
@@ -1,10 +1,16 @@
 (function(w) {
-    var RemoveTool = function(canvas, util) {
-        this.run = function() {
+    var RemoveTool = function(canvas, canvasTool) {
+        var remove = function() {
             if (canvas.getActiveObject()) {
                 canvas.remove(canvas.getActiveObject());
             }
         };
+        this.run = remove;
+        canvasTool.subscribe('RemoveTool', function(eventType, keyCode) {
+        	if (eventType === 'keydown' && keyCode === 46) {
+        		remove();
+        	}
+        });
         return this;
     };
     w.registerCanvasTool('remove', RemoveTool);
